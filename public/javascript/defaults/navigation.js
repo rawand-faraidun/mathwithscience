cc5cc566333be8f8e79615622547d6a43d1a794e
rawand.f <rawandmessi21@gmail.com>
2020-12-08 21:26:27 +0300

    changed navigation and home view for calculators

--- public/javascript/defaults/navigation.js
+++ public/javascript/defaults/navigation.js
@@ -1,19 +1,19 @@
 //this code is not for homepage, homepage Onscrol animation writen in its own JAVASCRIPT (home.js) file
 //scrolling the page
 window.onscroll = function () {
-    if (window.pageYOffset > 100){
+    if (window.pageYOffset > 100) {
         //when the width of the screen is bigger 768px
         if (window.innerWidth > 768) {
             document.getElementById("header").classList.add("header-scroll");
         }
-        else{
+        else {
             document.getElementById("header").classList.add("header-768-scroll");
             document.getElementById("nav").classList.add("nav-768-scroll");
             document.getElementById("closer").classList.add("closer-768-scroll");
         }
     }
     //getting everything to normal
-    else{
+    else {
         document.getElementById("header").classList.remove("header-scroll");
         document.getElementById("header").classList.remove("header-768-scroll");
         document.getElementById("nav").classList.remove("nav-768-scroll");
@@ -21,12 +21,28 @@ window.onscroll = function () {
     }
 };
 
-function navOpen(){
+function navOpen() {
     document.getElementById("nav").classList.toggle("nav-open");
-    document.getElementById("closer").classList.toggle("closer-open");
+    // if it was closed, and now opening
+    if (!document.getElementById("closer").classList.contains("closer-open")) {
+        document.getElementById("closer").classList.add("block");
+        setTimeout(function () {
+            document.getElementById("closer").classList.add("closer-open");
+        }, 0);
+    }
+    //if it was opened before, and now closing
+    else {
+        document.getElementById("closer").classList.remove("closer-open");
+        setTimeout(function () {
+            document.getElementById("closer").classList.remove("block");
+        }, 595);
+    }
 }
 
-function navClose(){
+function navClose() {
     document.getElementById("nav").classList.remove("nav-open");
     document.getElementById("closer").classList.remove("closer-open");
+    setTimeout(function () {
+        document.getElementById("closer").classList.remove("block");
+    }, 595);
 }
