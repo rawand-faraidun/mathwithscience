7bd94070d78e176c50594c2c4c8d3e1371e3bcc9
rawand.f <rawandmessi21@gmail.com>
2021-03-21 02:56:42 +0300

    added Header

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router /*,  Switch */ } from 'react-router-dom';
+
+// importing components
+import Header from './components/partials/Header/Header';
+
+function App() {
+
+  // removing the preload class from the body when the page is ready
+  // preload class prevent anything to animate before the whole page is ready
+  useEffect(() => {
+    document.getElementsByTagName('body')[0].removeAttribute('class');
+  }, []);
+
+
+  return (
+    <Router>
+
+      {/* Adding header of the page */}
+      <Header />
+
+
+      {/* this is a test element to make the page scroolable */}
+      <div className="test" id="test" style={{ position: 'absolute', top: 2000, backgroundColor: 'red', width: '100%' }}>
+        aajv
+                <br />
+                jhsd
+                <br />
+                isvaa
+            </div>
+
+    </Router>
+  );
+}
+
+export default App;
