d3cbe4c5ccb830c883bc53f781123fe5ca7dd94f
rawand.f <rawandmessi21@gmail.com>
2021-03-21 02:54:11 +0300

    changed App.js Location

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router /*,  Switch */ } from 'react-router-dom';
+
+// importing components
+import Header from './partials/Header/Header';
 
 function App() {
+
+  // removing the preload class from the body when the page is ready
+  // preload class prevent anything to animate before the whole page is ready
+  useEffect(() => {
+    document.getElementsByTagName('body')[0].removeAttribute('class');
+  }, []);
+
+
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <Router>
+
+      <Header />
+
+
+      <div className="test" id="test" style={{ position: 'absolute', top: 2000, backgroundColor: 'red', width: '100%' }}>
+        aajv
+                <br />
+                jhsd
+                <br />
+                isvaa
+            </div>
+
+    </Router>
   );
 }
 
