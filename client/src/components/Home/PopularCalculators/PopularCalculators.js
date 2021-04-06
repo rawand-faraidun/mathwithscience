91eab71d3360c6baf3ffe9c3df16f3d82f9b268a
rawand.f <rawandmessi21@gmail.com>
2021-04-06 23:50:13 +0300

    added kurdish language

--- client/src/components/Home/PopularCalculators/PopularCalculators.js
+++ client/src/components/Home/PopularCalculators/PopularCalculators.js
@@ -3,6 +3,64 @@ import React from 'react';
 // importing stylings
 import './popular-calculators.css';
 
+// importing components
+import languageHelper from '../../partials/languageHelper';
+
+
+
+// page content all languages
+const componentContent = {
+    title: {
+        en: 'Most Popular Calculators',
+        kr: 'ژمێرەرە زۆر بەکارهاتوەکان',
+    },
+    calculators: [
+        {
+            urlName: 'proportion-ratio',
+            en: { name: 'Proportion Ratio 1', description: 'lorem ipsum dolor sit amet consectetur adip game occurence lorem ipsum dolor sit amet consectetur adip game occurence' },
+            kr: { name: 'ڕێژەی تێکڕا 1', description: 'لۆرێم ئیپسوم دۆلۆر سیت ئامێت کۆنسێکتێتور لۆرێم ئیپسوم دۆلۆر سیت ئامێت کۆنسێکتێتور' }
+            , visited: 0
+        },
+        {
+            urlName: 'proportion-ratio',
+            en: { name: 'Proportion Ratio 2', description: 'lorem ipsum dolor sit amet consectetur adip game occurence lorem ipsum dolor sit amet consectetur adip game occurence' },
+            kr: { name: 'ڕێژەی تێکڕا 2', description: 'لۆرێم ئیپسوم دۆلۆر سیت ئامێت کۆنسێکتێتور لۆرێم ئیپسوم دۆلۆر سیت ئامێت کۆنسێکتێتور' }
+            , visited: 0
+        },
+        {
+            urlName: 'proportion-ratio',
+            en: { name: 'Proportion Ratio 3', description: 'lorem ipsum dolor sit amet consectetur adip game occurence lorem ipsum dolor sit amet consectetur adip game occurence' },
+            kr: { name: 'ڕێژەی تێکڕا 3', description: 'لۆرێم ئیپسوم دۆلۆر سیت ئامێت کۆنسێکتێتور لۆرێم ئیپسوم دۆلۆر سیت ئامێت کۆنسێکتێتور' }
+            , visited: 0
+        },
+        {
+            urlName: 'proportion-ratio',
+            en: { name: 'Proportion Ratio 4', description: 'lorem ipsum dolor sit amet consectetur adip game occurence lorem ipsum dolor sit amet consectetur adip game occurence' },
+            kr: { name: 'ڕێژەی تێکڕا 4', description: 'لۆرێم ئیپسوم دۆلۆر سیت ئامێت کۆنسێکتێتور لۆرێم ئیپسوم دۆلۆر سیت ئامێت کۆنسێکتێتور' }
+            , visited: 0
+        },
+        {
+            urlName: 'proportion-ratio',
+            en: { name: 'Proportion Ratio 5', description: 'lorem ipsum dolor sit amet consectetur adip game occurence lorem ipsum dolor sit amet consectetur adip game occurence' },
+            kr: { name: 'ڕێژەی تێکڕا 5', description: 'لۆرێم ئیپسوم دۆلۆر سیت ئامێت کۆنسێکتێتور لۆرێم ئیپسوم دۆلۆر سیت ئامێت کۆنسێکتێتور' }
+            , visited: 0
+        },
+        {
+            urlName: 'proportion-ratio',
+            en: { name: 'Proportion Ratio 6', description: 'lorem ipsum dolor sit amet consectetur adip game occurence lorem ipsum dolor sit amet consectetur adip game occurence' },
+            kr: { name: 'ڕێژەی تێکڕا 6', description: 'لۆرێم ئیپسوم دۆلۆر سیت ئامێت کۆنسێکتێتور لۆرێم ئیپسوم دۆلۆر سیت ئامێت کۆنسێکتێتور' }
+            , visited: 0
+        },
+        {
+            urlName: 'proportion-ratio',
+            en: { name: 'Proportion Ratio 7', description: 'lorem ipsum dolor sit amet consectetur adip game occurence lorem ipsum dolor sit amet consectetur adip game occurence' },
+            kr: { name: 'ڕێژەی تێکڕا 7', description: 'لۆرێم ئیپسوم دۆلۆر سیت ئامێت کۆنسێکتێتور لۆرێم ئیپسوم دۆلۆر سیت ئامێت کۆنسێکتێتور' }
+            , visited: 0
+        }
+    ]
+}
+
+
 
 /**
  *  @return : Element, returns popular calculators grid
@@ -10,33 +68,19 @@ import './popular-calculators.css';
  */
 function PopularCalculators() {
 
-    var calculators = [
-        { name: 'Proportion Ratio', description: 'lorem ipsum dolor sit amet consectetur adip game occurence lorem ipsum dolor sit amet consectetur adip game occurence', visited: 0 },
-        { name: 'Equation Solver', description: 'lorem ipsum dolor sit amet consectetur adip game occurence lorem ipsum dolor sit amet consectetur adip game occurence', visited: 0 },
-        { name: 'Decimal Fraction', description: 'lorem ipsum dolor sit amet consectetur adip game occurence lorem ipsum dolor sit amet consectetur adip game occurence', visited: 0 },
-        { name: 'Prime Checker', description: 'lorem ipsum dolor sit amet consectetur adip game occurence lorem ipsum dolor sit amet consectetur adip game occurence', visited: 0 },
-        { name: 'Volume', description: 'lorem ipsum dolor sit amet consectetur adip game occurence lorem ipsum dolor sit amet consectetur adip game occurence', visited: 0 },
-        { name: 'Date', description: 'lorem ipsum dolor sit amet consectetur adip game occurence lorem ipsum dolor sit amet consectetur adip game occurence', visited: 0 },
-        { name: 'Time', description: 'lorem ipsum dolor sit amet consectetur adip game occurence lorem ipsum dolor sit amet consectetur adip game occurence', visited: 0 },
-        { name: 'Lenght', description: 'lorem ipsum dolor sit amet consectetur adip game occurence lorem ipsum dolor sit amet consectetur adip game occurence', visited: 0 },
-        { name: 'Angle', description: 'lorem ipsum dolor sit amet consectetur adip game occurence lorem ipsum dolor sit amet consectetur adip game occurence', visited: 0 },
-        { name: 'Digital Storage', description: 'lorem ipsum dolor sit amet consectetur adip game occurence lorem ipsum dolor sit amet consectetur adip game occurence', visited: 0 },
-        { name: 'Wight', description: 'lorem ipsum dolor sit amet consectetur adip game occurence lorem ipsum dolor sit amet consectetur adip game occurence', visited: 0 },
-        { name: 'Time', description: 'lorem ipsum dolor sit amet consectetur adi occurence lorem ipsum dolor sit amet consectetur adip occurence', visited: 0 }
-    ]
-
-
     return (
-        <div className="popular-calculators">
+        <div className="popular-calculators" dir={languageHelper.getDirection()}>
 
-            <h2 className="popular-calculators-title">Most Popular Calculators</h2>
+            <h2 className={`popular-calculators-title ${languageHelper.getClass()}`}>{componentContent.title[languageHelper.getLanguageSymbol()]}</h2>
 
             <div className="popular-calculators-grid">
-                {calculators.map((calculator, i) => {
+                {componentContent.calculators.map((calculator, i) => {
                     return (
-                        <a href="/" className="calculator" key={i}>
-                            <h2 className="calculator-name">{calculator.name}</h2>
-                            <p className="calculator-description">{`${calculator.description.substring(0, 100)}...`}</p>
+                        <a href={`/all-calculators/${calculator.urlName}`} className="calculator" key={i}>
+                            <h2 className={`calculator-name ${languageHelper.getClass()}`}>{calculator[languageHelper.getLanguageSymbol()].name}</h2>
+                            <p className={`calculator-description ${languageHelper.getClass()}`}>
+                                {`${calculator[languageHelper.getLanguageSymbol()].description.substring(0, 100)}...`}
+                            </p>
                         </a>
                     );
                 })}
