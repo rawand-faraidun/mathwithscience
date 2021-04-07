a3c34aef4e7028e833c64f39b09c6342ee0ca763
rawand.f <rawandmessi21@gmail.com>
2021-04-07 18:02:36 +0300

    navigation links initial works

--- client/src/components/partials/Header/Navlinks/Navlinks.js
+++ client/src/components/partials/Header/Navlinks/Navlinks.js
@@ -12,47 +12,54 @@ import languageHelper from '../../languageHelper';
 // page content all languages
 const componentContent = {
     links: [
-        { en: 'Branches', kr: 'لقەکان', urlName: 'calculators' },
-        { en: 'All Calculators', kr: 'هەموو ژمێرەرەکان', urlName: 'all-calculators' }
+        {
+            en: 'Branches',
+            kr: 'لقەکان',
+            urlName: 'calculators'
+        },
+        {
+            en: 'All Calculators',
+            kr: 'هەموو ژمێرەرەکان',
+            urlName: 'all-calculators'
+        }
     ]
 }
 
 
-
 /**
  * @props :
- *      @whiteOnly : Boolean, this determines if the links must always be white or not
+ *      @whiteOnly {Boolean}, this determines if the links must always be white or not
  * 
- *  @return : Element, returns the navigation links
- *  @includes : navigation links
+ * @return {Element} : Header element
  */
 function Navlinks(props) {
 
-    // getting the current path of the website
     // used in chossing which link is gets the active class
     const thisPath = useLocation().pathname;
 
-
     return (
-        <div className={`nav-links ${props.navState ? 'nav-links-opened' : ''}`}>
-            <ul dir={languageHelper.getDirection()}>
+        <>
+            {/* navigation links */}
+            <div className={`nav-links ${props.navState ? 'nav-links-opened' : ''}`}>
 
-                {/* adding each link */}
-                {componentContent.links.map((link, i) =>
-                    <li
-                        className={`link 
+                {/* navigation links list */}
+                <ul dir={languageHelper.getDirection()}>
+
+                    {/* adding each link */}
+                    {componentContent.links.map((link, i) =>
+                        <li className={`link 
                             ${thisPath.startsWith(`/${link.urlName}`) ? "active" : ''} 
-                            ${props.whiteOnly ? "white" : ''}
-                        `}
-                    >
-                        <a href={`/${link.urlName}`} className={languageHelper.getClass()}>
-                            {link[languageHelper.getLanguageSymbol()]}
-                        </a>
-                    </li>
-                )}
+                            ${props.whiteOnly ? "white" : ''}`}
+                        >
+                            <a href={`/${link.urlName}`} className={languageHelper.getClass()}>
+                                {link[languageHelper.getLanguageSymbol()]}
+                            </a>
+                        </li>
+                    )}
 
-            </ul>
-        </div>
+                </ul>
+            </div>
+        </>
     )
 }
 
