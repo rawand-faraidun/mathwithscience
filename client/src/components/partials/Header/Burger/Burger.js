36fddc512ac5752074fc4650512b57956bbf8d0b
rawand.f <rawandmessi21@gmail.com>
2021-04-07 18:26:06 +0300

    burger initial works

--- client/src/components/partials/Header/Burger/Burger.js
+++ client/src/components/partials/Header/Burger/Burger.js
@@ -7,12 +7,11 @@ import './burger.css';
 
 /**
  * @props :
- *      @navState : Boolean, determines if the navigation is opened or not, controlled in Header
- *      @navToggleHandler : Function, handels the navigation drawer toggling, controlled in header
- *      @whiteOnly : Boolean, this determines if the burger lines must always be white or not
+ *      @navState : {Boolean}, determines if the navigation is opened or not, controlled in Header
+ *      @navToggleHandler {Function}, handels the navigation drawer toggling, controlled in header
+ *      @whiteOnly {Boolean}, this determines if the burger lines must always be white or not
  * 
- *  @return : Element, returns the burger used in navigation
- *  @includes : navigation burger
+ * @return {Element} : navigation burger
  */
 function Burger(props) {
 
@@ -20,21 +19,18 @@ function Burger(props) {
     function openBurger() {
         props.navToggleHandler(!props.navState);
     }
-
-    // this doesn't need state because when the element re-render it will update to
-    // making the lines white or not based on white only property
-    const burgerLinesStyling = {}
-    if (props.whiteOnly) {
-        burgerLinesStyling.backgroundColor = 'var(--white-color)';
-    }
-
-
+    
     return (
-        <div className={`nav-burger ${props.navState ? 'nav-bar-opened' : ''}`} onClick={openBurger}>
-            <div className="burger-line" style={burgerLinesStyling} />
-            <div className="burger-line" style={burgerLinesStyling} />
-            <div className="burger-line" style={burgerLinesStyling} />
-        </div>
+        <>
+            {/* navigation burger */}
+            <div className={`nav-burger ${props.navState ? 'nav-bar-opened' : ''}`} onClick={openBurger}>
+
+                {/* each lines in the burger */}
+                <div className={`burger-line ${props.whiteOnly ? 'burger-line-white' : ''}`} />
+                <div className={`burger-line ${props.whiteOnly ? 'burger-line-white' : ''}`} />
+                <div className={`burger-line ${props.whiteOnly ? 'burger-line-white' : ''}`} />
+            </div>
+        </>
     )
 }
 
