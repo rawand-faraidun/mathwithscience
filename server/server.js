85150666c325e66b4afca443ccfb66dff3978cb1
rawand.f <rawandmessi21@gmail.com>
2021-04-07 23:30:16 +0300

    set initial requirements and connected to local DB

--- server/server.js
+++ server/server.js
@@ -1,14 +1,35 @@
 // jshint esversion:9
 
 // setting requirements
-var express = require('express');
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // setting app
 const app = express();
 
 
+//connectiong to database
+mongoose.connect(
+    'mongodb://localhost:27017/MathwithScience',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+    .then(console.log("!!DB CONEECTED!!"))
+    .catch(err => { console.log(`DB Connection Error: ${err.message}`); });
+
+mongoose.set("useCreateIndex", true);
+
+
+// configuring app
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 // setting port
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), () => {
-    console.log(`[SERVER]: app started on port ${app.get('port')}`);
-})
+    console.log(`app started on port ${app.get('port')}`);
+});
