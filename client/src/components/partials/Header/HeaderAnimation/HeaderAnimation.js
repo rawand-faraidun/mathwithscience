f1b047b0c6dd3f7e1b14b6f6a1f373e1f39ea064
rawand.f <rawandmessi21@gmail.com>
2021-04-07 18:19:20 +0300

    header animation initial works

--- client/src/components/partials/Header/HeaderAnimation/HeaderAnimation.js
+++ client/src/components/partials/Header/HeaderAnimation/HeaderAnimation.js
@@ -6,15 +6,24 @@ import './header-animation.css';
 // importing libraries
 import anime from "animejs";
 
+
 /**
- *  @return : Element, returns the animation in the small screens header
- *  @includes : header Animation
+ * @return {Element} : Header navigation drawer animation
  */
 function HeaderAnimation() {
+
 
     // the animation rows and columns
     const animationRows = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
     const animationColumns = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+    // starting point of the animation, this will pass the first 11 columns
+    const animationStart = (animationRows.length * animationColumns.length) - ((animationRows.length * animationColumns.length) - (16 * animationColumns.length))
+
+    // end point of the animation
+    const animationEnd = (animationRows.length * animationColumns.length);
+
 
     // creating the animation after rendering the animation div
     useEffect(() => {
@@ -25,17 +34,18 @@ function HeaderAnimation() {
                 { value: 1, easing: 'easeInOutQuad', duration: 1200 }
             ],
             loop: true,
-            delay: anime.stagger(200, { grid: [10, 20], from: 'last' })
+            delay: anime.stagger(200, { grid: [10, 20], from: anime.random(animationStart, animationEnd) })
         });
-    }, []);
+    });
 
 
     return (
-        <div className="animation" id="animation">
-            {
-                // generating rows of the animation
-                animationRows.map((row, i) => {
-                    return (
+        <>
+            {/* the animation */}
+            <div className="animation" id="animation">
+                {
+                    // generating rows of the animation
+                    animationRows.map((row, i) =>
                         <div className="animation-row" id={`row${i}`} key={i}>
                             {
                                 // generation column squares in the animation
@@ -44,12 +54,10 @@ function HeaderAnimation() {
                                 })
                             }
                         </div>
-
                     )
-                })
-            }
-        </div>
-
+                }
+            </div>
+        </>
     )
 }
 
