34f2b49b4a4f5ce5841395884ef8a686e83e6f5f
rawand.f <rawandmessi21@gmail.com>
2021-04-09 02:00:59 +0300

    search now works for both branches and calculators

--- server/routes/search.js
+++ server/routes/search.js
@@ -5,6 +5,7 @@ const express = require("express");
 
 // getting model
 const Branches = require('../models/Branches');
+const Calculators = require('../models/Calculators');
 
 // setting router
 const router = express.Router();
@@ -16,16 +17,16 @@ const router = express.Router();
 /**
  * @param {req.body} : 
  *      { searchText } is the search key
- *      { limit } sets limit of how many result come back
+ *      { limit } OPTIONAL sets limit of how many result come back
  *      { language } determines to return which language
  * 
- * @param {res} : responds with array of branch and calculators
+ * @param {res} : responds with array of branch and calculators based on search
  */
+// the search is based on Title, Description or keywords of Branches or Caculators include the search text
 router.post("/", async (req, res) => {
 
     try {
         // finding branches
-        // the search is based on Title, Description or keywords include the search text
         // it will get { urlName, specified language details } of all branches and sort them by visitCount (DESC: big to small) then by their name (ASC)
         var branches = await Branches.find({
             $or: [
@@ -37,12 +38,24 @@ router.post("/", async (req, res) => {
             ]
         }, { _id: false, urlName: true, [req.body.language]: true }).sort({ visitCount: -1, 'en.name': 1 }).lean();
 
-        /** 
-         * @TODO : add parts to search also
-        */
+        // finding calculators
+        // it will get { urlName, specified language details } of all calculators and sort them by visitCount (DESC: big to small) then by their name (ASC)
+        var calculators = await Calculators.find({
+            $or: [
+                { 'en.name': new RegExp(req.body.searchText) },
+                { 'en.description': new RegExp(req.body.searchText) },
+                { 'kr.name': new RegExp(req.body.searchText) },
+                { 'kr.description': new RegExp(req.body.searchText) },
+                { keywords: { $in: new RegExp(req.body.searchText) } }
+            ]
+        }, { _id: false, urlName: true, [req.body.language]: true }).sort({ visitCount: -1, 'en.name': 1 }).lean();
+
 
         // sending the search result
-        res.status(200).send(branches);
+        res.status(200).send(
+            // concatination branches and calculators into one array and sending
+            [...branches, ...calculators].slice(0, req.body.limit ? +req.body.limit : Infinity) // if limit excists, then adding limit to returning results
+        );
 
     }
     catch (err) {
