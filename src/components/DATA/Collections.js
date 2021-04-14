dd77294cef852995b756d9180244d8ac3dd168cc
rawand.f <rawandmessi21@gmail.com>
2021-04-14 23:45:28 +0300

    moved Collections array to Collections-Data

--- src/components/DATA/Collections.js
+++ src/components/DATA/Collections.js
@@ -1,55 +1,8 @@
 // importing cloneDeep from lodash
 import cloneDeep from 'lodash/cloneDeep'
 
-/**
- *  @collections : array of collections
- */
-const Collections = [
-    {
-        en: {
-            name: "Algebra",
-            description: "Algebra is a branch of mathematics dealing with symbols and the rules for manipulating those symbols. In elementary algebra, those symbols (today written as Latin and Greek letters) represent quantities without fixed values, known as variables."
-        },
-        kr: {
-            name: "زانستی جەبر",
-            description: "زانستی جەبر لقێکە لە بیرکاری کە کاردەکات لەگەڵ هێماکان و یاساکانی چارەسەرکردنی ئەو هێمایانە. لە زانستی جەبری سەرەتایی، ئەو هێمایانە (لەئێستایا بە پیتی یۆنانی ئەنوسرێن) گوزارشت لە بڕی بەها ناجێگیر ئەکەن، کە ناسراون بە گۆڕدراو"
-        },
-        urlName: "algebra",
-        keywords: [
-            "algebra"
-        ]
-    },
-    {
-        en: {
-            name: "Geometry",
-            description: "Geometry is a branch of mathematics that studies the size, shape, position, angle, dimension and any other geometry properties of things."
-        },
-        kr: {
-            name: "ئەندازەگەری",
-            description: "ئەندازەگەری بەشێکە لە بیرکاری کە دەکۆڵێتەوە لە قەبارە، شێوە، شوێن، گۆشە، دووری و هەر تایبەتمەندیەکی تری ئەندازیاری شتەکان"
-        },
-        urlName: "geometry",
-        keywords: [
-            "geometry"
-        ]
-    },
-    {
-        en: {
-            name: "Unit Conversion",
-            description: "Conversion of Units is a multi-step process that converts units of measurement for the same quantity."
-        },
-        kr: {
-            name: "گۆڕانکاری یەکەکان",
-            description: "گۆڕانکاری یەکەکان پڕۆسەیەکی چەند هەنگاویە کە یەکەکان ئەگۆڕێت بۆ هەمان بڕ"
-        },
-        urlName: "unit-conversion",
-        keywords: [
-            "unit conversion"
-        ]
-    }
-];
-
-
+// importing Collections
+import Collections from './Collections-Data';
 
 
 
@@ -64,7 +17,8 @@ const Collections = [
 function find(conditions) {
 
     // cloning the collections array
-    var result = cloneDeep(Collections);
+    var result = cloneDeep(Collections());
+
 
     // checking for search query
     // it will make result to only return collections that include searchQuery in one of their properties
@@ -80,6 +34,8 @@ function find(conditions) {
             )
         });
     }
+
+    console.log(Collections());
 
     return result;
 }
