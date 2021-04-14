e978f5d5fb4870b7e84ab6ab0e5240ed5a41c37d
rawand.f <rawandmessi21@gmail.com>
2021-04-14 23:27:20 +0300

    added find to Calculators

--- src/components/DATA/Calculators.js
+++ src/components/DATA/Calculators.js
@@ -21,3 +21,45 @@ const Calculators = [
         ]
     }
 ];
+
+
+
+
+
+// getting collections
+/**
+ * @parameters :
+ *      @conditions : {Object}, adding conditions to modify returning array
+ *          @searchQuery : {String}, the search text that used to search for collections
+ * 
+ *  @return {Array} : search result grid element, if search was empty it will show collections
+ */
+function find(conditions) {
+
+    // cloning the collections array
+    var result = cloneDeep(Calculators);
+
+    // checking for search query
+    // it will make result to only return collections that include searchQuery in one of their properties
+    if (conditions?.searchQuery) {
+        result = result.filter(calculator => {
+
+            // returning the ones that english or kurdish name or description includes the search text
+            return (
+                calculator.en.name.toLowerCase().includes(conditions.searchQuery.toLowerCase()) ||
+                calculator.en.description.toLowerCase().includes(conditions.searchQuery.toLowerCase()) ||
+                calculator.kr.name.toLowerCase().includes(conditions.searchQuery.toLowerCase()) ||
+                calculator.kr.description.toLowerCase().includes(conditions.searchQuery.toLowerCase())
+            )
+        });
+    }
+
+    return result;
+}
+
+
+
+
+
+// exporting module
+export { find };
