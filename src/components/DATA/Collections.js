74161cb0e54dce1529a7b59e919c4d63eca36117
rawand.f <rawandmessi21@gmail.com>
2021-04-14 22:57:46 +0300

    added find to Collections

--- src/components/DATA/Collections.js
+++ src/components/DATA/Collections.js
@@ -48,3 +48,45 @@ const Collections = [
         ]
     }
 ];
+
+
+
+
+
+// getting collections
+/**
+ * @parameters :
+ *      @conditions : {Object}, adding conditions to modify returning array
+ *          @searchQuery : {String}, the search text that used to search for collections
+ * 
+ *  @return {Array} : search result grid element, if search was empty it will show collections
+ */
+function find(conditions) {
+
+    // cloning the collections array
+    var result = cloneDeep(Collections);
+
+    // checking for search query
+    // it will make result to only return collections that include searchQuery in one of their properties
+    if (conditions?.searchQuery) {
+        result = result.filter(collection => {
+
+            // returning the ones that english or kurdish name or description includes the search text
+            return (
+                collection.en.name.toLowerCase().includes(conditions.searchQuery.toLowerCase()) ||
+                collection.en.description.toLowerCase().includes(conditions.searchQuery.toLowerCase()) ||
+                collection.kr.name.toLowerCase().includes(conditions.searchQuery.toLowerCase()) ||
+                collection.kr.description.toLowerCase().includes(conditions.searchQuery.toLowerCase())
+            )
+        });
+    }
+
+    return result;
+}
+
+
+
+
+
+// exporting module
+export { find };
