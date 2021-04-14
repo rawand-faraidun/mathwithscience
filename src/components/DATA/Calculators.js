f980ef48a79f767db4b853d919932a77e7b9da8d
rawand.f <rawandmessi21@gmail.com>
2021-04-14 23:26:54 +0300

    added Calculators to DATA

--- src/components/DATA/Calculators.js
+++ src/components/DATA/Calculators.js
@@ -7,86 +7,17 @@ import cloneDeep from 'lodash/cloneDeep'
 const Calculators = [
     {
         en: {
-            name: "Algebra",
-            description: "Algebra is a branch of mathematics dealing with symbols and the rules for manipulating those symbols. In elementary algebra, those symbols (today written as Latin and Greek letters) represent quantities without fixed values, known as variables."
+            "name": "Proportion Ratio",
+            "description": "Proportion Ratio states that two fractions on each side of an equation are equal."
         },
         kr: {
-            name: "زانستی جەبر",
-            description: "زانستی جەبر لقێکە لە بیرکاری کە کاردەکات لەگەڵ هێماکان و یاساکانی چارەسەرکردنی ئەو هێمایانە. لە زانستی جەبری سەرەتایی، ئەو هێمایانە (لەئێستایا بە پیتی یۆنانی ئەنوسرێن) گوزارشت لە بڕی بەها ناجێگیر ئەکەن، کە ناسراون بە گۆڕدراو"
+            "name": "ڕێژەی تێکڕا",
+            "description": "ڕێژەی تێکڕا پشت ڕاست دەکاتەوە کە دوو کەرت لە هەردوولای هاوکێشەیەک یەکسانن"
         },
-        urlName: "algebra",
+        branchUrl: "algebra",
+        urlName: "proportion-ratio",
         keywords: [
-            "algebra"
-        ]
-    },
-    {
-        en: {
-            name: "Geometry",
-            description: "Geometry is a branch of mathematics that studies the size, shape, position, angle, dimension and any other geometry properties of things."
-        },
-        kr: {
-            name: "ئەندازەگەری",
-            description: "ئەندازەگەری بەشێکە لە بیرکاری کە دەکۆڵێتەوە لە قەبارە، شێوە، شوێن، گۆشە، دووری و هەر تایبەتمەندیەکی تری ئەندازیاری شتەکان"
-        },
-        urlName: "geometry",
-        keywords: [
-            "geometry"
-        ]
-    },
-    {
-        en: {
-            name: "Unit Conversion",
-            description: "Conversion of Units is a multi-step process that converts units of measurement for the same quantity."
-        },
-        kr: {
-            name: "گۆڕانکاری یەکەکان",
-            description: "گۆڕانکاری یەکەکان پڕۆسەیەکی چەند هەنگاویە کە یەکەکان ئەگۆڕێت بۆ هەمان بڕ"
-        },
-        urlName: "unit-conversion",
-        keywords: [
-            "unit conversion"
+            "proportion ratio"
         ]
     }
 ];
-
-
-
-
-
-// getting collections
-/**
- * @parameters :
- *      @conditions : {Object}, adding conditions to modify returning array
- *          @searchQuery : {String}, the search text that used to search for collections
- * 
- *  @return {Array} : search result grid element, if search was empty it will show collections
- */
-function find(conditions) {
-
-    // cloning the collections array
-    var result = cloneDeep(Calculators);
-
-    // checking for search query
-    // it will make result to only return collections that include searchQuery in one of their properties
-    if (conditions?.searchQuery) {
-        result = result.filter(calculator => {
-
-            // returning the ones that english or kurdish name or description includes the search text
-            return (
-                calculator.en.name.toLowerCase().includes(conditions.searchQuery.toLowerCase()) ||
-                calculator.en.description.toLowerCase().includes(conditions.searchQuery.toLowerCase()) ||
-                calculator.kr.name.toLowerCase().includes(conditions.searchQuery.toLowerCase()) ||
-                calculator.kr.description.toLowerCase().includes(conditions.searchQuery.toLowerCase())
-            )
-        });
-    }
-
-    return result;
-}
-
-
-
-
-
-// exporting module
-export { find };
