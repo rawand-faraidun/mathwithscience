7c8ec8b4b5578eeae3a86824edcd4cd1cac90292
rawand.f <rawandmessi21@gmail.com>
2021-05-17 03:48:49 +0300

    added findIn function

--- src/components/DATA/Calculators.js
+++ src/components/DATA/Calculators.js
@@ -78,11 +78,13 @@ function find(conditions) {
 
 
 
-// getting calculators
+// getting one calculator
 /**
  * @parameters :
  *      @conditions : {Object}, adding conditions to find returning object
  *          @urlName : {String}, searches for the object based on urlName property
+ *          @language : {Boolean}, determines if the returning calculator to be in one language or all languages
+ *          @changeUrl : {String}, determines if adding a text before urlName or not
  * 
  *  @return {Array} : search result grid element, if search was empty it will show calculators
  */
@@ -130,7 +132,57 @@ function findOne(conditions) {
 
 
 
+// getting calculators
+/**
+ * @parameters :
+ *      @conditions : {Object}, adding conditions to find returning object
+ *          @list : {Array}, Array of urlNames to search in for calculators
+ *          @language : {Boolean}, determines if only returning results in one language or all languages
+ *          @changeUrl : {String}, determines if adding a text before urlName or not
+ * 
+ *  @return {Array} : search result grid element, if search was empty it will show calculators
+ */
+function findIn(conditions) {
+
+    // cloning the calculators array
+    var result = cloneDeep(Calculators());
+
+
+    // checking for limit
+    // it will limit the results
+    if (conditions?.list) {
+        result = result.filter(calculator => conditions.list.includes(calculator.urlName))
+    }
+
+
+    // checking for language
+    // it will make the result to only return details of one language
+    if (conditions?.language) {
+        result.forEach(calculator => {
+            // removing other language details from the object
+            delete calculator[languageHelper.getOtherLanguageSymbol()];
+            return calculator
+        });
+    }
+
+
+    // checking for changingUrl
+    // it will add the choosen text before url of each calculator
+    if (conditions?.changeUrl) {
+        result.forEach(calculator => {
+            // adding the text before the url
+            calculator.urlName = `${conditions.changeUrl}/${calculator.urlName}`;
+            return calculator
+        });
+    }
+
+
+    return result;
+}
+
+
+
 
 
 // exporting module
-export { find, findOne };
+export { find, findOne, findIn };
