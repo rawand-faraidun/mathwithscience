1b6032ad58fcfbf72b6fd348b43c2bf7897fe476
rawand-faraidun <rawandmessi21@gmail.com>
2021-08-04 02:21:30 +0300

    added express server

---
+++ server/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+
+// setting app
+const app = express()
+
+
+// configuring app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+
+
+
+
+// serving app frontend
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+
+
+
+// setting and listening to post
+app.set('port', process.env.PORT || 5000)
+app.listen(app.get('port'), (err) => {
+    if (err) { console.log(`!!!SERVER LISTENING ERROR: ${err}`) }
+    console.log(`server listening on port ${app.get('port')}`)
+})
