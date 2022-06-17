1dfbc7e915178c9dc79888c5b1b22064dd32dc16
Rawand <201.rawand@gmail.com>
2022-06-17 19:35:04 +0300

    fix homepage meta tags

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 import HomeMain from './HomeMain/HomeMain'
 import PopularCalculators from './PopularCalculators/PopularCalculators'
@@ -13,6 +14,18 @@ export default function Home() {
 
     return (
         <>
+            {/* overriding document head */}
+            <Helmet>
+                <title>Math with Science | free helping website with math and science problems</title>
+                <meta name="description" content="Math with Science is a website to help anyone with theit math problems." />
+                <meta name="keywords" content="math with science, math, science, online calculator" />
+
+                <meta property="og:title" content="Math with Science | free helping website with math and science problems" />
+                <meta property="og:description" content="Math with Science is a website to help anyone with theit math problems." />
+                <meta property="og:url" content={window.location.href} />
+            </Helmet>
+
+
             {/* homepage */}
             <div>
 
